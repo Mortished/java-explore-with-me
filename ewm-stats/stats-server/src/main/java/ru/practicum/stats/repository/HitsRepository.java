872034edd3d62390ffd5d333207c77eec6ba7cd3
s.kukhtanov <s.kukhtanov@tinkoff.ru>
2024-05-22 23:15:56 +0300

    stage-1: fix order in queries

--- ewm-stats/stats-server/src/main/java/ru/practicum/stats/repository/HitsRepository.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/stats/repository/HitsRepository.java
@@ -12,22 +12,28 @@ import ru.practicum.stats.entity.Hit;
 public interface HitsRepository extends JpaRepository<Hit, Long> {
 
   @Query(value = "select h.app, h.uri, count(h.*) as hits from hits h "
-      + "where timestamp between ?1 and ?2 group by h.uri, h.app", nativeQuery = true)
+      + "where timestamp between ?1 and ?2 "
+      + "group by h.uri, h.app "
+      + "order by count(h.*) desc", nativeQuery = true)
   List<IStatsDTO> findHits(LocalDateTime start, LocalDateTime end);
 
   @Query(value = "select h.app, h.uri, count(distinct h.ip) as hits from hits h "
-      + "where timestamp between ?1 and ?2 group by h.uri, h.app", nativeQuery = true)
+      + "where timestamp between ?1 and ?2 "
+      + "group by h.uri, h.app "
+      + "order by count(distinct h.ip) desc", nativeQuery = true)
   List<IStatsDTO> findDistinctHits(LocalDateTime start, LocalDateTime end);
 
   @Query(value = "select h.app, h.uri, count(distinct h.ip) as hits from hits h "
       + "where timestamp between ?1 and ?2 "
       + "and h.uri in (?3) "
-      + "group by h.uri, h.app", nativeQuery = true)
+      + "group by h.uri, h.app "
+      + "order by count(distinct h.ip)", nativeQuery = true)
   List<IStatsDTO> findDistinctHitsByUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
   @Query(value = "select h.app, h.uri, count(h.*) as hits from hits h "
       + "where timestamp between ?1 and ?2 "
       + "and h.uri in (?3) "
-      + "group by h.uri, h.app", nativeQuery = true)
+      + "group by h.uri, h.app "
+      + "order by count(h.*) desc", nativeQuery = true)
   List<IStatsDTO> findHitsByUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
