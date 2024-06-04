f278ab41ffc62aa32a9d77faaf1d558f519f0dce
s.kukhtanov <s.kukhtanov@tinkoff.ru>
2024-06-04 20:25:42 +0300

    stage-2: fix query v3

--- ewm-service/service/src/main/java/ru/practicum/repository/EventRepository.java
+++ ewm-service/service/src/main/java/ru/practicum/repository/EventRepository.java
@@ -10,7 +10,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
   @Query(value = "SELECT * "
       + "FROM events "
-      + "WHERE (id IN (?1) OR ?1 IS null) "
+      + "WHERE (user_id IN (?1) OR ?1 IS null) "
       + "AND (state IN (?2) OR ?2 IS null) "
       + "AND (category_id IN (?3) OR ?3 IS null) "
       + "AND (event_date > ?4 OR ?4 IS null) "
@@ -18,9 +18,6 @@ public interface EventRepository extends JpaRepository<Event, Long> {
       + "LIMIT ?7 OFFSET ?6 ", nativeQuery = true)
   List<Event> findByAllParams(List<Long> users, List<String> states, List<Long> categories,
       LocalDateTime rangeStart, LocalDateTime rangeEnd, Integer from, Integer size);
-
-  @Query(value = "SELECT * FROM events LIMIT ?2 OFFSET ?1", nativeQuery = true)
-  List<Event> findAllWithPaging(Integer from, Integer size);
 
   @Query(value = "SELECT * FROM events WHERE user_id = ?1 LIMIT ?3 OFFSET ?2", nativeQuery = true)
   List<Event> findAllByUserId(Long userId, Integer from, Integer size);
